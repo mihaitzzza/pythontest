9632b5800df22dad3926498204a138e2df255d6a
Mihai Vladu <vladuomihai@gmail.com>
2023-01-09 10:12:47 +0200

    Add src layout.

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,6 +1,6 @@
-from pythontest.functions import my_sum
+from src.pythontest.functions import my_sum
 
 
 def test_my_sum():
-	result = my_sum(2, 5)
-	assert result == 7
+    result = my_sum(2, 5)
+    assert result == 7
